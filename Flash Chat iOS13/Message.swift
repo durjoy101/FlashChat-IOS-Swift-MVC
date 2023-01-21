8d6c3138e2c224b7f302000ed0a3bc97f6fdf334
durjoy101 <durjoyahmed101@gmail.com>
2023-01-21 15:42:27 +0600

    initial commit

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,15 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Samar Mitra on 4/12/22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    var sender: String
+    var body: String
+    
+}
---
+++ Flash Chat iOS13/Views/TestCell.swift
@@ -0,0 +1,24 @@
+//
+//  TestCell.swift
+//  Flash Chat iOS13
+//
+//  Created by Samar Mitra on 6/12/22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class TestCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
